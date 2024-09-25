a21fd0d70cfbc78dcf6acc2d8f287493ddd52ecf
postai13 <142340559+postai13@users.noreply.github.com>
2024-09-25 11:30:33 -0300

    egetg

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function soma(num13,num22){
 }
 console.log (soma(13,22))
 
-function soma(num13,num22){
-    return num13*num22;
+function soma(num10,num23){
+    return num10*num23;
 }
-console.log (soma(13,22))
+console.log (soma(10,23))
