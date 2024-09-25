822f1e49238d8d5c9dc0b7c46b37f75da62bc0f2
postai13 <142340559+postai13@users.noreply.github.com>
2024-09-25 11:45:06 -0300

    fgnfg

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function soma(num1,num2){
 }
 console.log (soma(13,22))
 
-function multi(num3,num4){
+function quadrado(num3,num4){
     return num3*num4;
 }
-console.log (multi(13,13))
+console.log (quadrado(13,13))
