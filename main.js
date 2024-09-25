9c66fa3e69edfb52b774aa567257f005b5132df4
postai13 <142340559+postai13@users.noreply.github.com>
2024-09-25 11:35:44 -0300

    bgn

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 alert('Guilherme 12 e Juliano 17')
-function soma(num13,num22){
-    return num13+num22;
+function soma(num1,num2){
+    return num1+num2;
 }
 console.log (soma(13,22))
 
-function soma(num1,num2){
-    return num1*num2;
+function soma(num3,num4){
+    return num3*num4;
 }
-console.log (soma(1,2))
+console.log (soma(13,22))
