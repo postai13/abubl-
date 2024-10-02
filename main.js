bf562e1617bfd4a01486068f8127395f6903147e
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-02 11:56:45 -0300

    jgjtugj

--- main.js
+++ main.js
@@ -19,9 +19,10 @@
 // console.log ((3+3+6)/3)
 
 let estados=['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
-let V=0;
 
+let V=0;
 while (V<5) {
 console.log(V);
+console.log(estados[V]);
 V=V+1;
 }
