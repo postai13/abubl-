83801dd6377bc8559b77154266a9b5c4e74bc33a
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-02 11:31:05 -0300

    fhnfdn

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log (soma(13,22))
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log (soma(13,22))
 
-function quadrado(num3,num4){
-    return num3*num4;
-}
-console.log (quadrado(13,13))
+// function quadrado(num3,num4){
+//     return num3*num4;
+// }
+// console.log (quadrado(13,13))
 
-function quadradox(lado,){
-    return lado**2;
-}
-console.log (quadradox(13))
+// function quadradox(lado,){
+//     return lado**2;
+// }
+// console.log (quadradox(13))
 
-let media=function(num5,num6,num7){
-    return num5+num6+num7/3;
-}
-console.log ((3+3+6)/3)
+// let media=function(num5,num6,num7){
+//     return num5+num6+num7/3;
+// }
+// console.log ((3+3+6)/3)
+
+let estados=['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
