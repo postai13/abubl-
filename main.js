0869ed2c0b1b7cba79a653ccc8c3a89a54e4deca
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-02 11:54:30 -0300

    rhry

--- main.js
+++ main.js
@@ -19,3 +19,9 @@
 // console.log ((3+3+6)/3)
 
 let estados=['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
+let V=0;
+
+while (V<5) {
+console.log(V);
+V=V+1;
+}
