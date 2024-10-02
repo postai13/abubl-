4b29c4a2eea1540725ea59b1173da5274bc96ed8
postai13 <142340559+postai13@users.noreply.github.com>
2024-10-02 12:20:43 -0300

    iljilij

--- main.js
+++ main.js
@@ -20,9 +20,13 @@
 
 let estados=['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
 
-let V=0;
+let =0;
 while (V<estados.length) {
 console.log(V);
 console.log(estados[V]);
 V=V++;
+}
+for (let  = 0; V < estados.length; V++) {
+    console.log(V);
+    console.log(estados[V]);   
 }
